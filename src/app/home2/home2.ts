b337c16e8c1c1e3c762b3c852178eb27d0c45b61
oSlashh <tarunanal89@gmail.com>
2026-06-03 15:16:13 +0530

    home page final

--- src/app/home2/home2.ts
+++ src/app/home2/home2.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home2',
+  standalone: true,
   imports: [],
   templateUrl: './home2.html',
   styleUrl: './home2.css',
 })
-export class Home2 {}
+export class Home2 {
+  login(){
+    console.log("Login clicked");
+  }
+  register(){
+    console.log("Register clicked");
+  }
+}
