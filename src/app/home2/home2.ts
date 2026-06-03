76ac466e431b4a05a8100db8f84d2c48a0435eb8
oSlashh <tarunanal89@gmail.com>
2026-06-03 10:40:09 +0530

    home page creation

---
+++ src/app/home2/home2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home2',
+  imports: [],
+  templateUrl: './home2.html',
+  styleUrl: './home2.css',
+})
+export class Home2 {}
