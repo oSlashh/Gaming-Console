9c1f03b5f75dd3bcdfdcc02978fc2164549c3a52
oSlashh <tarunanal89@gmail.com>
2026-06-05 15:16:44 +0530

    game page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { Home2 } from './home2/home2';
 import { Login } from './login/login';
 import { Registration } from './registration/registration';
+import { GamePage } from './game-page/game-page';
 
 export const routes: Routes = [
   { path: '', component: Home2 },
   { path: 'login', component: Login },
-  { path: 'register', component: Registration }
+  { path: 'register', component: Registration },
+  { path: 'game-page', component: GamePage }
 ];
